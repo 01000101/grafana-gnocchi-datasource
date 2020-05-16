dd69006c36c588bc6723e2b6e52533a9400d8b04
Joshua Cornutt <joshua@cloudify.co>
2020-05-17 00:37:47 +0200

    adding keystone endpoint

--- dist/specs/gnocchi-datasource-specs.js
+++ dist/specs/gnocchi-datasource-specs.js
@@ -895,7 +895,7 @@ describe('GnocchiDatasource', function () {
         beforeEach(function () {
             ds = new module_1.Datasource({
                 'url': 'http://localhost:5000',
-                'jsonData': { 'mode': 'keystone', 'username': 'user', 'project': 'proj', 'password': 'pass', 'domain': 'foo' }
+                'jsonData': { 'mode': 'keystone', 'username': 'user', 'project': 'proj', 'password': 'pass', 'domain': 'foo', 'endpoint': 'foo' }
             }, $q, backendSrv, templateSrv);
             $httpBackend.expect('POST', "http://localhost:5000/v3/auth/tokens", { "auth": { "identity": { "methods": ["password"],
                         "password": { "user": { "name": "user", "password": "pass", "domain": { "id": "foo" } } } },
